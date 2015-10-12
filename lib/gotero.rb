153ab174c5c0d4176cb1a7947caa1a03f8061d47
Eloy Espinaco <eloyesp@gmail.com>
2015-10-12 16:51:49 -0300

    Move files.

--- lib/gotero.rb
+++ lib/gotero.rb
@@ -1,29 +1,3 @@
-require 'pp'
-
-class Knowledge
-  def ask string
-    q = Question.new "?"
-    ! q.well_formed?
-  end
-end
-
-class Question
-  def initialize string
-  end
-
-  def well_formed?
-  end
-end
-
-expected_output = <<SEQUML
-t->knowledge: ask (1)
-knowledge->question: initialize (?)
-question->knowledge:
-knowledge->question: well_formed? ()
-question->knowledge:
-knowledge->t:
-SEQUML
-
 class Gotero
   attr_accessor :output
 
@@ -63,15 +37,4 @@ class Gotero
     end
     "#{ current_emiter }->#{ subject }:#{ message_details }"
   end
-end
-
-gotero = Gotero.new
-
-gotero.trace do 
-  Knowledge.new.ask 1
-end
-
-# assert_equal expected_output.lines.size, gotero.output.lines.size
-expected_output.lines.zip(gotero.output.lines).each do |expected, actual|
-  assert_equal actual, expected
 end
---
+++ test/simple_trace.rb
@@ -0,0 +1,35 @@
+require_relative '../lib/gotero'
+
+class Knowledge
+  def ask string
+    q = Question.new "?"
+    ! q.well_formed?
+  end
+end
+
+class Question
+  def initialize string
+  end
+
+  def well_formed?
+  end
+end
+
+expected_output = <<SEQUML
+t->knowledge: ask (1)
+knowledge->question: initialize (?)
+question->knowledge:
+knowledge->question: well_formed? ()
+question->knowledge:
+knowledge->t:
+SEQUML
+
+gotero = Gotero.new
+
+gotero.trace do 
+  Knowledge.new.ask 1
+end
+
+expected_output.lines.zip(gotero.output.lines).each do |expected, actual|
+  assert_equal actual, expected
+end
